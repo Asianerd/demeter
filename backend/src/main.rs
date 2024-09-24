e03bc49136ce2f7bbbdd9223af000f09880d514e
ajian_nedo <asianerd28@gmail.com>
2024-09-24 11:14:59 +0800

    Request creation, editing, deletion and fetching

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,6 +9,7 @@ mod desk;
 mod dish;
 mod species;
 
+mod session;
 mod request;
 
 #[get("/")]
@@ -28,11 +29,14 @@ async fn rocket() -> _ {
         .mount("/table/create", routes![desk::create])
         .mount("/table/delete", routes![desk::delete])
         .mount("/table/fetch", routes![desk::fetch])
+        .mount("/table/fetch_all", routes![desk::fetch_all])
 
         .mount("/species/create", routes![species::create])
         .mount("/species/delete", routes![species::delete])
         .mount("/species/edit", routes![species::edit])
         .mount("/species/fetch", routes![species::fetch])
+        .mount("/species/fetch_all", routes![species::fetch_all])
+        .mount("/species/fetch_by_name", routes![species::fetch_by_name])
 
         .mount("/dish/create", routes![dish::create])
         .mount("/dish/delete", routes![dish::delete])
@@ -40,5 +44,18 @@ async fn rocket() -> _ {
         .mount("/dish/fetch", routes![dish::fetch])
         .mount("/dish/fetch_all", routes![dish::fetch_all])
 
-        
+        .mount("/request/create", routes![request::create])
+        .mount("/request/delete", routes![request::delete])
+        .mount("/request/edit", routes![request::edit])
+        .mount("/request/fetch", routes![request::fetch])
+
+        .mount("/session/start", routes![session::start])
+        .mount("/session/end", routes![session::end])
+        .mount("/session/end_by_desk", routes![session::end_by_desk])
+        .mount("/session/change_desk", routes![session::change_desk])
+        .mount("/session/change_desk_by_desk", routes![session::change_desk_by_desk])
+        .mount("/session/fetch", routes![session::fetch])
+        .mount("/session/fetch_open", routes![session::fetch_open])
+        .mount("/session/fetch_all", routes![session::fetch_all])
+        .mount("/session/fetch_requests", routes![session::fetch_requests])
 }
--- backend/src/request.rs
+++ backend/src/request.rs
@@ -1,8 +1,10 @@
 use rocket::State;
-use sqlx::{Row, sqlite::SqliteRow, Pool, Sqlite};
+use serde::{Deserialize, Serialize};
+use sqlx::{prelude::FromRow, Pool, Sqlite};
 
 use crate::{callback_result::Result, utils::ValueString};
 
+#[derive(FromRow, Debug, Serialize, Deserialize)]
 pub struct Request {
     pub id: i32,
     pub session: i32,
@@ -11,6 +13,9 @@ pub struct Request {
     pub size: i32,
     pub comment: String,
     pub state: i32
+    // 0 -> pending
+    // 1 -> in kitchen
+    // 2 -> completed
 }
 impl Request {
     pub async fn check_variant(db: &Pool<Sqlite>, dish: i32, variant: i32) -> bool {
@@ -64,9 +69,86 @@ impl Request {
 
         Result::Success
     }
+
+    pub async fn edit(db: &Pool<Sqlite>, request_id: i32, variant: i32, size: i32, comment: String, state: i32) -> Result {
+        let dish;
+        match Request::fetch(&db, request_id).await {
+            Some(r) => {
+                dish = r;
+            },
+            None => {
+                return Result::DoesntExist;
+            }
+        }
+
+        if !Request::check_variant(db, dish.id, variant).await {
+            return Result::VariantDoesntExist;
+        }
+
+        if !Request::check_size(db, dish.id, size).await {
+            return Result::SizeDoesntExist;
+        }
+
+        sqlx::query("update request set variant = $1, size = $2, comment = $3, state = $4 where id = $5;")
+            .bind(variant)
+            .bind(size)
+            .bind(comment)
+            .bind(state)
+            .bind(dish.id)
+            .execute(db)
+            .await
+            .unwrap();
+
+        Result::Success
+    }
+
+    pub async fn fetch(db: &Pool<Sqlite>, request_id: i32) -> Option<Request> {
+        match sqlx::query_as::<_, Request>("select * from request where id = $1;")
+        .bind(request_id)
+        .fetch_one(db)
+        .await {
+            Ok(r) => Some(r),
+            Err(e) => {
+                println!("request.rs; fetch({request_id}); error: {e}");
+                None
+            }
+        }
+    }
+
+    pub async fn delete(db: &Pool<Sqlite>, request_id: i32) -> Result {
+        match Request::fetch(db, request_id).await {
+            Some(_) => {
+                sqlx::query("delete from request where id = $1;")
+                    .bind(request_id)
+                    .execute(db)
+                    .await
+                    .unwrap();
+
+                Result::Success
+            },
+            None => Result::DoesntExist
+        }
+    }
 }
 
-//  session: i32, dish: i32, variant: i32, size: i32, comment: String, state: i32)
-// #[get("")]
-// pub async fn create(db: &State<Pool<Sqlite>>) -> String {}
+#[get("/<session>/<dish>/<variant>/<size>/<comment>/<state>")]
+pub async fn create(db: &State<Pool<Sqlite>>, session: i32, dish: i32, variant: i32, size: i32, comment: String, state: i32) -> String {
+    Request::create(&db, session, dish, variant, size, comment, state).await.to_string()
+}
+
+#[get("/<request_id>/<variant>/<size>/<comment>/<state>")]
+pub async fn edit(db: &State<Pool<Sqlite>>, request_id: i32, variant: i32, size: i32, comment: String, state: i32) -> String {
+    Request::edit(db.inner(), request_id, variant, size, comment, state).await.to_string()
+}
+
+#[get("/<request_id>")]
+pub async fn fetch(db: &State<Pool<Sqlite>>, request_id: i32) -> String {
+    serde_json::to_string(&Request::fetch(db.inner(), request_id).await).unwrap()
+}
+
+#[get("/<request_id>")]
+pub async fn delete(db: &State<Pool<Sqlite>>, request_id: i32) -> String {
+    Request::delete(db.inner(), request_id).await.to_string()
+}
+
 
