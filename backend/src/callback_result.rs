c4fa396c0b29db3385c72bbbebfbf056fe0d6879
ajian_nedo <asianerd28@gmail.com>
2024-09-24 11:13:22 +0800

    Singular fetch + fetching only open

--- backend/src/callback_result.rs
+++ backend/src/callback_result.rs
@@ -7,6 +7,9 @@ pub enum Result {
     DoesntExist,
     Exists,
 
+    TableOccupied,
+    TableUnoccupied,
+
     VariantDoesntExist,
     SizeDoesntExist
 }
--- backend/src/desk.rs
+++ backend/src/desk.rs
@@ -2,7 +2,7 @@ use rocket::State;
 use serde::{Deserialize, Serialize};
 use sqlx::{prelude::FromRow, Pool, Sqlite};
 
-use crate::{callback_result::Result, utils::ValueInt};
+use crate::{callback_result::Result, session::Session, utils::ValueInt};
 
 #[derive(FromRow, Debug, Serialize, Deserialize)]
 pub struct Desk {
@@ -11,24 +11,19 @@ pub struct Desk {
 }
 impl Desk {
     pub async fn create(db: &Pool<Sqlite>, name: String, capacity: i32) -> Result {
-        let result = sqlx::query_as::<_, ValueInt>("select * from desk where name = $1;")
-            .bind(&name)
-            .fetch_one(db)
-            .await
-            .unwrap();
+        match Desk::fetch(db, &name).await {
+            Some(_) => Result::Exists,
+            None => {
+                sqlx::query("insert into desk values($1, $2);")
+                    .bind(&name)
+                    .bind(capacity)
+                    .execute(db)
+                    .await
+                    .unwrap();
 
-        if result.0 >= 1 {
-            return Result::Exists;
+                Result::Success
+            }
         }
-
-        sqlx::query("insert into desk values($1, $2);")
-            .bind(name)
-            .bind(capacity)
-            .execute(db)
-            .await
-            .unwrap();
-
-            Result::Success
     }
 
     pub async fn delete(db: &Pool<Sqlite>, name: String) -> Result {
@@ -51,25 +46,59 @@ impl Desk {
         Result::Success
     }
 
-    pub async fn fetch(db: &Pool<Sqlite>) -> Vec<Desk> {
+    pub async fn fetch(db: &Pool<Sqlite>, name: &String) -> Option<Desk> {
+        match sqlx::query_as("select * from desk where name = $1;")
+            .bind(&name)
+            .fetch_one(db)
+            .await {
+            Ok(d) => Some(d),
+            Err(e) => {
+                println!("desk.rs; fetch({name}); error : {e}");
+                None
+            }
+        }
+    }
+
+    pub async fn fetch_all(db: &Pool<Sqlite>) -> Vec<Desk> {
         sqlx::query_as("select * from desk;")
             .fetch_all(db)
             .await
             .unwrap()
     }
+
+
+    pub async fn fetch_open_session(db: &Pool<Sqlite>, desk: &String) -> Option<Session> {
+        match sqlx::query_as("select * from session where desk = $1 and state = 1;")
+            .bind(&desk)
+            .fetch_one(db)
+            .await {
+            Ok(s) => {
+                Some(s)
+            },
+            Err(e) => {
+                println!("desk.rs; fetch_session({desk}); error: {e}");
+                None
+            }
+        }
+    }
 }
 
 #[get("/<name>/<capacity>")]
 pub async fn create(db: &State<Pool<Sqlite>>, name: String, capacity: i32) -> String {
-    Desk::create(db.inner(), name, capacity).await.to_string()
+    Desk::create(db.inner(), urlencoding::decode(&name).unwrap().to_string(), capacity).await.to_string()
 }
 
 #[get("/<name>")]
 pub async fn delete(db: &State<Pool<Sqlite>>, name: String) -> String {
-    Desk::delete(db.inner(), name).await.to_string()
+    Desk::delete(db.inner(), urlencoding::decode(&name).unwrap().to_string()).await.to_string()
+}
+
+#[get("/<name>")]
+pub async fn fetch(db: &State<Pool<Sqlite>>, name: String) -> String {
+    serde_json::to_string(&Desk::fetch(db.inner(), &urlencoding::decode(&name).unwrap().to_string()).await).unwrap()
 }
 
 #[get("/")]
-pub async fn fetch(db: &State<Pool<Sqlite>>) -> String {
-    serde_json::to_string(&Desk::fetch(db.inner()).await).unwrap()
+pub async fn fetch_all(db: &State<Pool<Sqlite>>) -> String {
+    serde_json::to_string(&Desk::fetch_all(db.inner()).await).unwrap()
 }
